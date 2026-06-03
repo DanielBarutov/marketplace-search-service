4647a5a377af221900b6f888779325ac93b6ef11
Daniel Barutov <barutovdg@gmail.com>
2026-06-03 13:30:02 +0300

    deploy to LMS

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,14 @@
 FROM python:3.13-slim-bookworm
 
+COPY --from=ghcr.io/astral-sh/uv:latest /uv /uvx /bin/
+
 ENV PYTHONUNBUFFERED=1 \
     PYTHONDONTWRITEBYTECODE=1 \
     UV_LINK_MODE=copy \
     UV_COMPILE_BYTECODE=1 \
     UV_NO_DEV=1 \
     UV_FROZEN=1 \
-    PYTHONPATH=/app \
-    PATH="/root/.local/bin:$PATH"
-
-RUN apt-get update \
-    && apt-get install -y --no-install-recommends curl ca-certificates \
-    && rm -rf /var/lib/apt/lists/* \
-    && curl -LsSf https://astral.sh/uv/install.sh | sh
+    PYTHONPATH=/app
 
 WORKDIR /app
 
@@ -23,6 +19,6 @@ COPY . .
 
 RUN uv sync --frozen --no-dev
 
-EXPOSE 8003
+EXPOSE 8000
 
-CMD ["uv", "run", "python", "-m", "bin.api"]
+CMD ["sh", "-c", "uv run alembic upgrade head && uv run python -m bin.api"]
--- src/settings.py
+++ src/settings.py
@@ -1,13 +1,28 @@
+import os
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
-
-    database_url: str = (
-        "postgresql+asyncpg://postgres:postgres@localhost:5435/search_db"
+    still_url: str | None = os.getenv("POSTGRES_CONNECTION_STRING")
+    if still_url:
+        database_url: str = still_url.replace("postgres:", "postgresql+asyncpg:", 1)
+    else:
+        database_url: str = (
+            "postgresql+asyncpg://postgres:postgres@localhost:5435/search_db"
+        )
+    kafka_bootstrap_servers: str = (
+        os.getenv("KAFKA_BROKERS") if os.getenv("KAFKA_BROKERS") else "localhost:9092"
     )
-    kafka_bootstrap_servers: str = "localhost:9092"
-    kafka_topic_ads: str = "ads"
+    kafka_topic_ads: str = (
+        os.getenv("KAFKA_TOPIC_MARKETPLACE_ADS")
+        if os.getenv("KAFKA_TOPIC_MARKETPLACE_ADS")
+        else "ads"
+    )
     kafka_consumer_group: str = "search-service"
-    ad_service_url: str = "http://localhost:8002"
+    ad_service_url: str = (
+        os.getenv("AD_SERVICE_URL")
+        if os.getenv("AD_SERVICE_URL")
+        else "http://localhost:8002"
+    )
